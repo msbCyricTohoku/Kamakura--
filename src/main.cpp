e16ac290480f593fc5ae203db1e3c648e550946f
M.S. Ben <142302587+msbCyricTohoku@users.noreply.github.com>
2025-06-06 19:51:00 +0800

    Use Qt resource path for window icon

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,9 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     kamakura w;
-    w.setWindowIcon(QIcon("resources/icon.ico"));
+    // Use the resource system so the application icon is bundled in the binary
+    // The path corresponds to the prefix defined in resources.qrc
+    w.setWindowIcon(QIcon(":/new/prefix1/resources/icon.ico"));
     w.show();
     return a.exec();
 }
