2efa2e89bc751600f4d12e9fdca930e4319bb49e
msbCyricTohoku <ben.sh@tohoku.ac.jp>
2026-02-21 09:55:53 +0800

    added argument openFile support

--- src/kamakura.h
+++ src/kamakura.h
@@ -1,16 +1,18 @@
 #ifndef KAMAKURA_H
 #define KAMAKURA_H
 
-#include <QMainWindow>
-#include <QList>
-#include <QStringList>
-#include <QMenu>
-#include <QWheelEvent>
 #include "language.h"
+#include <QList>
+#include <QMainWindow>
+#include <QMenu>
+#include <QStringList>
+#include <QWheelEvent>
 
 // Forward Declarations
 QT_BEGIN_NAMESPACE
-namespace Ui { class kamakura; }
+namespace Ui {
+class kamakura;
+}
 QT_END_NAMESPACE
 class QTabWidget;
 class Highlighter;
@@ -26,118 +28,107 @@ class QDropEvent;
 class QAction;
 class QLabel;
 
+enum class Theme { Light, Dark, SolarizedLight, SolarizedDark };
 
-enum class Theme {
-    Light,
-    Dark,
-    SolarizedLight,
-    SolarizedDark
-};
+// Kamakura-- Mehrdad S. Beni and Hiroshi Watabe, Japan 2023
 
-
-//Kamakura-- Mehrdad S. Beni and Hiroshi Watabe, Japan 2023
-
-class kamakura : public QMainWindow
-{
-    Q_OBJECT
+class kamakura : public QMainWindow {
+  Q_OBJECT
 
 public:
-   explicit kamakura(QWidget *parent = nullptr);
-    ~kamakura();
+  void openFileByPath(const QString &path);
+  explicit kamakura(QWidget *parent = nullptr);
+  ~kamakura();
 
 protected:
-    void dragEnterEvent(QDragEnterEvent* drag_event) override;
-    void dropEvent(QDropEvent* drop_event) override;
+  void dragEnterEvent(QDragEnterEvent *drag_event) override;
+  void dropEvent(QDropEvent *drop_event) override;
 
 private slots:
-    // Slots named to match the .ui file for auto-connection
-    void on_actionNew_triggered();
-    void on_actionOpen_triggered();
-    void on_actionSave_triggered();
-    void on_actionSave_2_triggered();
-    void on_actionQuit_triggered();
-    void on_actionCut_triggered();
-    void on_actionCopy_triggered();
-    void on_actionPaste_triggered();
-    void on_actionZoom_triggered();
-    void on_actionZoom_2_triggered();
-    void wheelEvent(QWheelEvent *event);
-    void on_actionSearch_and_Replace_triggered();
-    void on_actionGoToLine_triggered();
-    void on_actionDuplicate_Line_triggered();
-    void on_actionToggle_Comment_triggered();
-    void on_actionInsert_DateTime_triggered();
-    void on_actionKamakura_triggered();
-    void on_actionHowTo_triggered();
-     void on_actionTrim_Trailing_Spaces_triggered();
+  // Slots named to match the .ui file for auto-connection
+  void on_actionNew_triggered();
+  void on_actionOpen_triggered();
+  void on_actionSave_triggered();
+  void on_actionSave_2_triggered();
+  void on_actionQuit_triggered();
+  void on_actionCut_triggered();
+  void on_actionCopy_triggered();
+  void on_actionPaste_triggered();
+  void on_actionZoom_triggered();
+  void on_actionZoom_2_triggered();
+  void wheelEvent(QWheelEvent *event);
+  void on_actionSearch_and_Replace_triggered();
+  void on_actionGoToLine_triggered();
+  void on_actionDuplicate_Line_triggered();
+  void on_actionToggle_Comment_triggered();
+  void on_actionInsert_DateTime_triggered();
+  void on_actionKamakura_triggered();
+  void on_actionHowTo_triggered();
+  void on_actionTrim_Trailing_Spaces_triggered();
 
-    void setLightTheme();
-    void setDarkTheme();
-    void setSolarizedLightTheme();
-    void setSolarizedDarkTheme();
+  void setLightTheme();
+  void setDarkTheme();
+  void setSolarizedLightTheme();
+  void setSolarizedDarkTheme();
 
+  void toggleWordWrap(bool enabled);
+  void toggleLineNumbers(bool enabled);
+  void setLanguage(Language lang);
 
-
-    void toggleWordWrap(bool enabled);
-    void toggleLineNumbers(bool enabled);
-    void setLanguage(Language lang);
-
-    // Internal Logic Slots
-    void openFileByPath(const QString& path);
-    void closeTab(int index);
-    void updateTabDirtyStatus();
-    void onCurrentTabChanged(int index);
-    void syncListSelectionWithTab(int index);
-    void syncTabSelectionWithList(QListWidgetItem* item);
-    void handleTabMoved(int from, int to);
-
+  // Internal Logic Slots
+  // void openFileByPath(const QString &path);
+  void closeTab(int index);
+  void updateTabDirtyStatus();
+  void onCurrentTabChanged(int index);
+  void syncListSelectionWithTab(int index);
+  void syncTabSelectionWithList(QListWidgetItem *item);
+  void handleTabMoved(int from, int to);
 
 private:
-    Ui::kamakura *ui;
-    QTabWidget* tabs;
-    Highlighter* highlighter;
-    FindDialog *findDialog;
-    MetricReporter *metricReporter;
-    QLabel* syntaxLabel{nullptr};
-    bool hasCurrentHighlighting{false};
+  Ui::kamakura *ui;
+  QTabWidget *tabs;
+  Highlighter *highlighter;
+  FindDialog *findDialog;
+  MetricReporter *metricReporter;
+  QLabel *syntaxLabel{nullptr};
+  bool hasCurrentHighlighting{false};
 
+  // bool darkThemeEnabled{true};
+  // bool darkThemeEnabled{false};
+  //     bool wordWrapEnabled{true};
 
-    //bool darkThemeEnabled{true};
-    //bool darkThemeEnabled{false};
-    //    bool wordWrapEnabled{true};
+  Theme currentTheme{Theme::Light};
+  bool wordWrapEnabled{true};
+  bool lineNumbersEnabled{true};
 
-    Theme currentTheme{Theme::Light};
-    bool wordWrapEnabled{true};
-     bool lineNumbersEnabled{true};
+  QAction *wordWrapAction{nullptr};
+  QAction *lineNumbersAction{nullptr};
 
-    QAction* wordWrapAction{nullptr};
-    QAction* lineNumbersAction{nullptr};
-    
-    QAction* englishAction{nullptr};
-    QAction* japaneseAction{nullptr};
-    QMenu* languageMenu{nullptr};
-    
-    QDockWidget* opened_docs_dock;
-    QListWidget* opened_docs_widget;
+  QAction *englishAction{nullptr};
+  QAction *japaneseAction{nullptr};
+  QMenu *languageMenu{nullptr};
 
-    Language currentLanguage{Language::English};
+  QDockWidget *opened_docs_dock;
+  QListWidget *opened_docs_widget;
 
-        static constexpr int MaxRecentFiles = 5;
-    QStringList recentFiles;
-    QMenu* recentFilesMenu{nullptr};
+  Language currentLanguage{Language::English};
 
-    void addRecentFile(const QString& path);
-    void updateRecentFilesMenu();
-    void loadRecentFiles();
-    void saveRecentFiles();
-    void updateSyntaxLabel(const QString& extension, bool hasHighlighting);
-    QString syntaxNameForExtension(const QString& extension) const;
+  static constexpr int MaxRecentFiles = 5;
+  QStringList recentFiles;
+  QMenu *recentFilesMenu{nullptr};
 
-    void setupEditor(CodeEditor* editor);
-    void setupDocks();
-    void setupConnections();
-    void updateWindowTitle(const QString& currentFile);
-    CodeEditor* currentEditor();
-    QString trLang(const QString& en, const QString& ja) const;
+  void addRecentFile(const QString &path);
+  void updateRecentFilesMenu();
+  void loadRecentFiles();
+  void saveRecentFiles();
+  void updateSyntaxLabel(const QString &extension, bool hasHighlighting);
+  QString syntaxNameForExtension(const QString &extension) const;
+
+  void setupEditor(CodeEditor *editor);
+  void setupDocks();
+  void setupConnections();
+  void updateWindowTitle(const QString &currentFile);
+  CodeEditor *currentEditor();
+  QString trLang(const QString &en, const QString &ja) const;
 };
 #endif // KAMAKURA_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,45 @@
 #include "kamakura.h"
 
 #include <QApplication>
-#include <QPalette>
 #include <QColor>
-//Kamakura-- Mehrdad S. Beni and Hiroshi Watabe, Japan 2023
-static void applyDarkTheme(QApplication& app)
-{
-    app.setStyle("Fusion");
-    QPalette darkPalette;
-    darkPalette.setColor(QPalette::Window, QColor(53,53,53));
-    darkPalette.setColor(QPalette::WindowText, Qt::white);
-    darkPalette.setColor(QPalette::Base, QColor(42,42,42));
-    darkPalette.setColor(QPalette::AlternateBase, QColor(66,66,66));
-    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
-    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
-    darkPalette.setColor(QPalette::Text, Qt::white);
-    darkPalette.setColor(QPalette::Button, QColor(53,53,53));
-    darkPalette.setColor(QPalette::ButtonText, Qt::white);
-    darkPalette.setColor(QPalette::BrightText, Qt::red);
-    darkPalette.setColor(QPalette::Highlight, QColor(142,45,197).lighter());
-    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
-    app.setPalette(darkPalette);
+#include <QPalette>
+#include <QStringList>
+// Kamakura-- Mehrdad S. Beni and Hiroshi Watabe, Japan 2023
+static void applyDarkTheme(QApplication &app) {
+  app.setStyle("Fusion");
+  QPalette darkPalette;
+  darkPalette.setColor(QPalette::Window, QColor(53, 53, 53));
+  darkPalette.setColor(QPalette::WindowText, Qt::white);
+  darkPalette.setColor(QPalette::Base, QColor(42, 42, 42));
+  darkPalette.setColor(QPalette::AlternateBase, QColor(66, 66, 66));
+  darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+  darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+  darkPalette.setColor(QPalette::Text, Qt::white);
+  darkPalette.setColor(QPalette::Button, QColor(53, 53, 53));
+  darkPalette.setColor(QPalette::ButtonText, Qt::white);
+  darkPalette.setColor(QPalette::BrightText, Qt::red);
+  darkPalette.setColor(QPalette::Highlight, QColor(142, 45, 197).lighter());
+  darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+  app.setPalette(darkPalette);
 }
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    applyDarkTheme(a);
-    kamakura w;
-    // Use the resource system so the application icon is bundled in the binary
-    // The path corresponds to the prefix defined in resources.qrc
-    w.setWindowIcon(QIcon(":/new/prefix1/resources/icon.ico"));
-    w.show();
-    return a.exec();
+int main(int argc, char *argv[]) {
+  QApplication a(argc, argv);
+  applyDarkTheme(a);
+  kamakura w;
+
+  QStringList args = a.arguments();
+
+  if (args.count() > 1) {
+    for (int i = 1; i < args.count(); ++i) {
+      w.openFileByPath(args[i]);
+    }
+  }
+
+  // Use the resource system so the application icon is bundled in the binary
+  // The path corresponds to the prefix defined in resources.qrc
+  w.setWindowIcon(QIcon(":/new/prefix1/resources/icon.ico"));
+  w.show();
+  return a.exec();
 }
-//Kamakura-- Mehrdad S. Beni and Hiroshi Watabe, Japan 2023
+// Kamakura-- Mehrdad S. Beni and Hiroshi Watabe, Japan 2023
